8c792386caefca373dfac8ebe2efbd45fdca56d3
Sanya Sharma <sanya6018.be21@chitkara.edu.in>
2023-12-26 21:31:33 +0530

    Login and Registration Pages Added

---
+++ client/src/pages/Login.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {Button, Form , Input} from 'antd';
+import axios from "axios";
+import toast from "react-hot-toast";
+function Login() {
+  const navigate = useNavigate();
+  const onFinish = async(values) => {
+    try{
+      const response = await axios.post('/api/user/register',values);
+      if(response.data.success){
+        toast.success(response.data.message);
+        toast("Redirecting to home page");
+        localStorage.setItem("token",response.data.token);
+        navigate("/");
+      }else{
+        toast.error(response.data.message);
+      }
+    }catch(error){
+      toast.error('something went wrong');
+    }
+  };
+  return (
+    <div className='authentication'>
+    <div className='authentication-form card p-3'>
+    <h1 className='card-title'>Welcome back</h1>
+    <Form layout='vertical' onFinish={onFinish}>
+      <Form.Item label='Email' name='email'>
+      <Input placeholder='Email'/>
+      </Form.Item>
+      <Form.Item label='Password' name='password'>
+      <Input placeholder='Password' type='password'/>
+      </Form.Item>
+      <Button className='primary-button my-3' htmlType='submit'>
+        Login
+      </Button>
+      <Link to='/register' className='anchor mt-2'>CLICK HERE TO REGISTER</Link>
+    </Form>
+    </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+require('dotenv').config();
+const dbConfig = require("./config/dbconfig");
+app.use(express.json());
+const userRoute = require("./routes/userRoute");
+app.use('/api/user',userRoute);
+const port = process.env.PORT || 5000;
+// console.log(process.env.MONGO_URL);
+app.listen(port,() => console.log(`Listening on port ${port}`));
